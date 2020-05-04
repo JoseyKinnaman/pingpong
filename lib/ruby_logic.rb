5d9e32079fb3f6db0a10d26277debd9734fecb28
Mark Gridley <mark_gridley@yahoo.com>
2020-05-04 11:27:17 -0700

    write logic and create third test

--- lib/ruby_logic.rb
+++ lib/ruby_logic.rb
@@ -1,9 +1,24 @@
-#!/usr/bin/env ruby
+  #!/usr/bin/env ruby
   require('pry')
+#  puts "Enter a number!"
+#  number = gets.chomp.to_i
+#  puts ping_pong(number)
 
-
-
-def ping_pong(number)
-  array = []
-  x = 0
-end
+  def ping_pong(number)
+    array = []
+    x = 0
+    while (x < number.to_i)
+      # puts "x is: #{x}"
+      x = x +1
+      if x % 3 == 0 && x % 5 == 0
+        array.push('ping pong')
+      elsif x % 3 == 0
+        array.push('ping')
+      elsif x % 5 == 0
+        array.push('pong')
+      else
+        array.push(x)
+      end
+    end
+    array     
+  end
--- spec/ruby_logic_spec.rb
+++ spec/ruby_logic_spec.rb
@@ -1,11 +1,14 @@
 require('rspec')
 require('ruby_logic')
 
-describe('#ping_pong') do
+describe('ping_pong') do
   it("returns 'ping' if a number is divisible by 3") do 
-    expect(ping_pong(3)).to(eq('ping'))
+    expect(ping_pong(3)).to(eq([1, 2,'ping']))
   end
   it("returns 'pong' if a number is divisible by 5") do
-    expect(ping_pong(5)).to(eq('pong'))
+    expect(ping_pong(5)).to(eq([1, 2, 'ping', 4, 'pong']))
+  end
+  it("returns 'ping pong' if a number is divisible by 3 and 5") do
+    expect(ping_pong(15)).to(eq([1, 2, 'ping', 4, 'pong', 'ping', 7, 8, 'ping', 'pong', 11, 'ping', 13, 14, 'ping pong']))
   end
 end
